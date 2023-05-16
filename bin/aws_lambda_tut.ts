b90a0c1ac95454b97ea6cdbb8b6d653f5e62f64b
Jabreal Johnson <jabrealthecoder@gmail.org>
2023-05-16 09:20:28 -0400

    added gitignore and lambda

--- bin/aws_lambda_tut.ts
+++ bin/aws_lambda_tut.ts
@@ -2,20 +2,14 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsLambdaTutStack } from '../lib/aws_lambda_tut-stack';
+import {Aws} from "aws-cdk-lib";
 
 const app = new cdk.App();
 new AwsLambdaTutStack(app, 'AwsLambdaTutStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    env: {
+        account: Aws.ACCOUNT_ID,
+        region: Aws.REGION
+    }
 });
+
+app.synth()
--- lib/aws_lambda_tut-stack.ts
+++ lib/aws_lambda_tut-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsLambdaTutStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsLambdaTutQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, "jj-pipeline", {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('synth', {
+        input: CodePipelineSource.gitHub('jabrealmoe/aws_lambda_tut', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    })
   }
 }
